03288868f6d6580c5330565dfce47a7d29634d2e
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-04-02 19:30:20 -0300

    Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Header.scss'
+import CTA from './CTA'
+import Me from '../../Assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>John F.</h1>
+        <h5 className="text-light">Frontend Develloper</h5>
+        <CTA />
+        <HeaderSocials />
+        
+        <div className="me">
+          <img src={Me} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        
+      </div>
+    </header>
   )
 }
 
