e86e51dd2b7a7988c964d46c608ad182d858bd0a
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-08-23 17:48:54 -0300

    last stretch

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
+import { AiTwotoneMail } from 'react-icons/ai'
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 import './Contact.scss'
 
+const API_KEY = process.env.REACT_APP_EMAIL
 const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_o9dm3ps', 'template_8kn4tbs', form.current, API_KEY);
+
+    e.target.reset();
+  };
   return (
     <section id="contact">
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiTwotoneMail className='contact__icon' />
+            <h4>Email</h4>
+            <h5>jfschallibaum@gmail.com</h5>
+            <a href="mailto:jfschallibaum@gmail.com" target="_blank" rel="noopener noreferrer">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <BsMessenger className='contact__icon' />
+            <h4>Messenger</h4>
+            <h5>Juan's FB Messenger</h5>
+            <a href="https://m.me/SonicFran" target="_blank" rel="noopener noreferrer">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <IoLogoWhatsapp className='contact__icon' />
+            <h4>WhatsApp Messenger</h4>
+            <h5>+54 9 1123864313</h5>
+            <a href="https://api.whatsapp.com/send?phone=1123864313" target="_blank" rel="noopener noreferrer">Send a Message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your full name' required />
+          <input type="email" name="email" placeholder='Email address' required/>
+          <textarea name="message" rows="10" placeholder='Your message...' required></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
+import {
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+
 const Footer = () => {
   return (
     <footer id="footer">
-      Footer
-    </footer>
-  )
-}
+      <a href="#" className="footer__logo">
+        John.F
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.instagram.com/jfscha/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineInstagram />
+        </a>
+        <a
+          href="https://twitter.com/JohnFScha"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineTwitter />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/john-f-schallibaum-886530225/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineLinkedin />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; John.F All Rights Reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
