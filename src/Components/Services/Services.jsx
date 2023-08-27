f64f4056858ca962f7fed94ba576421a46c6d383
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-08-26 23:07:59 -0300

    added funtionality

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiCheck } from "react-icons/bi";
 import "./Services.scss";
 
 const Services = () => {
+  const [showList1, setShowList1] = useState(false);
+  const [showList2, setShowList2] = useState(false);
+  const [showList3, setShowList3] = useState(false);
+
+  const toggleList1 = () => {
+    setShowList1((prev) => !prev);
+  };
+
+  const toggleList2 = () => {
+    setShowList2((prev) => !prev);
+  };
+
+  const toggleList3 = () => {
+    setShowList3((prev) => !prev);
+  };
+
   return (
     <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
       <div className="container services__container">
         <article className="service">
-          <div className="service__head">
+          <div className="service__head" onClick={toggleList1}>
             <h3>Backend services</h3>
           </div>
-          <ul className="service__list">
+          <ul className={`service__list ${showList1 ? "show" : ""}`}>
             <li>
               <BiCheck className="service__icon" />
               <p>Database setting and maintanance.</p>
@@ -32,10 +48,10 @@ const Services = () => {
           </ul>
         </article>
         <article className="service">
-          <div className="service__head">
+          <div className="service__head" onClick={toggleList2}>
             <h3>Web development</h3>
           </div>
-          <ul className="service__list">
+          <ul className={`service__list ${showList2 ? "show" : ""}`}>
             <li>
               <BiCheck className="service__icon" />
               <p>Landing pages.</p>
@@ -63,10 +79,10 @@ const Services = () => {
           </ul>
         </article>
         <article className="service">
-          <div className="service__head">
+          <div className="service__head" onClick={toggleList3}>
             <h3>Soft Skills</h3>
           </div>
-          <ul className="service__list">
+          <ul className={`service__list ${showList3 ? "show" : ""}`}>
             <li>
               <BiCheck className="service__icon" />
               <p>Fast learner.</p>
